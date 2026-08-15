8a14260b118effc73489143896175b3c7c5315f4
René Jochum <rene@jochum.dev>
2026-08-15 23:57:37 +0200

    fix(client): point the build-image test at an image that actually exists
    Signed-off-by: René Jochum <rene@jochum.dev>

--- client/resource_image_test.go
+++ client/resource_image_test.go
@@ -198,7 +198,7 @@ func TestImageEnsure(t *testing.T) {
 		},
 		{
 			name:  "with create github",
-			image: "ghcr.io/linuxcontainers/alpine:latest",
+			image: "ghcr.io/ghcr-library/busybox:glibc",
 			opts:  []Option{OptionCreate()},
 		},
 		{
